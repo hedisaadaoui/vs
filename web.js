04ec8918cc3b77dda84a4af624c909e675ed7dc8
hedisaadaoui <jihedsaadaoui805@gmail.com>
2023-11-29 09:29:53 +0100

    hh

---
+++ web.js
@@ -0,0 +1,14 @@
+function verif(){
+    var x=document.getElementById("nom").innerHTML
+    var y=document.getElementById("prenom").innerHTML
+    if(x.value==""){
+        alert("Pensez à taper un message !");
+      
+    }
+    else if(y==""){
+        alert("ecrire votre prenom mmm")
+    }
+    if()
+    
+}
+
