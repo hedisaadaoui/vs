fa9b01e878ced8673db3739b1d1b263432e0a028
hedisaadaoui <jihedsaadaoui805@gmail.com>
2023-11-30 13:31:30 +0100

    hahah

---
+++ j.js
@@ -0,0 +1,47 @@
+function ex1(n,n1){
+    if(n<n1){
+        return n
+    }
+    else{ 
+    return (n%n1)
+    }
+}
+function ex2(array){
+    for (var i=0;i<array[i].length;i++){
+        typeof array[i]=='number'
+        return array[0]
+
+    }
+}
+function ex3(n){
+    var s=0
+    for(var i=0;i<n;i++){ 
+    s=s+i
+    }
+    return s
+
+}
+function ex4(array){
+    for (var i=0;i<array[i].length;i++){
+        
+    }
+}
+function ex5(n){
+    return Math.abs(n)
+}    
+function ex6(array1,array2){
+
+}
+function ex7(){
+
+}
+function ex8(){
+
+}
+function ex9(){
+
+}
+function ex10(){
+
+}
+
